1ffce652865026e9e1d5abb68de9388f9ce2ff1c
Frank Rousseau <frank.rousseau@free.fr>
2016-09-18 19:16:20 +0200

    Make code pep8 compliant

--- openfoodfacts/products.py
+++ openfoodfacts/products.py
@@ -1,17 +1,19 @@
+# -*- coding: utf-8 -*-
 import utils
 
 
 SEARCH_PATH = "cgi/search.pl"
 
-"""
-Return information of a given product.
-"""
+
 def get_product(barcode):
+    """
+    Return information of a given product.
+    """
     return utils.fetch('api/v0/product/%s' % barcode)
 
 
-"""
-Perform a search using Open Food Facts search engine.
-"""
 def search(query, pagination=20):
+    """
+    Perform a search using Open Food Facts search engine.
+    """
     pass
