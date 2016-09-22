286b047afe5099159d0baef1613e09c3049d1fc5
Frank Rousseau <frank.rousseau@free.fr>
2016-09-22 21:48:56 +0200

    Add capability to retrieve products by facet

--- tests/products_test.py
+++ tests/products_test.py
@@ -15,6 +15,20 @@ class TestProducts(unittest.TestCase):
             res = openfoodfacts.get_product('1223435')
             self.assertEquals(res, {'name': 'product_test'})
 
+    def test_get_by_trace(self):
+        with requests_mock.mock() as mock:
+            mock.get('http://world.openfoodfacts.org/trace/egg.json',
+                     text='{"products":["omelet"]}')
+            res = openfoodfacts.products.get_by_trace('egg')
+            self.assertEquals(res, ["omelet"])
+
+    def test_get_by_trace(self):
+        with requests_mock.mock() as mock:
+            mock.get('http://world.openfoodfacts.org/country/france.json',
+                     text='{"products":["omelet"]}')
+            res = openfoodfacts.products.get_by_country('france')
+            self.assertEquals(res, ["omelet"])
+
 
 if __name__ == '__main__':
     unittest.main()
