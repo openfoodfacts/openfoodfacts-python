3119e3a51632d96b88639d275be6e339ca46a92b
teolemon <teolemon@users.noreply.github.com>
2016-10-04 16:15:22 +0200

    Create autosuggest.py

---
+++ openfoodfacts/autosuggest.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+def get_autosuggest_url(tag_type, language, geography):
+	autosuggest = "http://" + geography + ".openfoodfacts.org/cgi/suggest.pl?lc="+ language + "&tagtype=" + tag_type
+	return autosuggest
