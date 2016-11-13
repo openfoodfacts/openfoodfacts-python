2b03f6b7b4b5a5155d9e7304593cafaf75706076
Frank Rousseau <frank.rousseau@free.fr>
2016-11-13 16:18:45 +0100

    Add a basic search feature

--- openfoodfacts/utils.py
+++ openfoodfacts/utils.py
@@ -3,10 +3,15 @@ import requests
 API_URL = "http://world.openfoodfacts.org/"
 
 
-def fetch(path):
+def fetch(path, json_file=True):
     """
     Fetch data at a given path assuming that target match a json file and is
     located on the OFF API.
     """
-    response = requests.get("%s%s.json" % (API_URL, path))
+    if json_file:
+        path = "%s%s.json" % (API_URL, path)
+    else:
+        path = "%s%s" % (API_URL, path)
+
+    response = requests.get(path)
     return response.json()
