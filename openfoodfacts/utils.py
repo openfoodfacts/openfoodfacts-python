3f863181319326a4a2755c6c8b39d0435efa048f
Frank Rousseau <frank.rousseau@free.fr>
2016-11-14 19:03:17 +0100

    Fix get_ocr_json_url_method (proper pep8)

--- openfoodfacts/utils.py
+++ openfoodfacts/utils.py
@@ -16,12 +16,22 @@ def fetch(path, json_file=True):
     response = requests.get(path)
     return response.json()
 
-def get_ocr_json_url_for_an_image(first_three_digits, second_three_digits, third_three_digits,fourth_three_digits, image_name):
+
+def get_ocr_json_url_for_an_image(first_three_digits,
+                                  second_three_digits,
+                                  third_three_digits,
+                                  fourth_three_digits,
+                                  image_name):
     """
-    Get the URL of a JSON file given a barcode in 4 chunks of 3 digits and an image name (1, 2, 3, front_fr…)
+    Get the URL of a JSON file given a barcode in 4 chunks of 3 digits and an
+    image name (1, 2, 3, front_fr...).
     """
-	try:
-		image_ocr_json = "http://world.openfoodfacts.org/images/products/" + str(first_three_digits)+ "/" + str(second_three_digits)+ "/" + str(third_three_digits)+ "/" + str(fourth_three_digits)+ "/" + str(image_name) + ".json"
-		return str(image_ocr_json)
-	except IndexError:
-		return None
+    url = "http://world.openfoodfacts.org/images/products/"
+    url += "%s/%s/%s/%s/%s.json" % (
+        first_three_digits,
+        second_three_digits,
+        third_three_digits,
+        fourth_three_digits,
+        image_name
+    )
+    return url
