1ccf95161df08c8ab1f16278cd4c00a6c1f04ff9
teolemon <teolemon@users.noreply.github.com>
2017-01-08 13:38:03 +0100

    Update autosuggest.py

--- openfoodfacts/autosuggest.py
+++ openfoodfacts/autosuggest.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 def get_autosuggest_url(tag_type, language, geography):
-	autosuggest = "http://" + geography + ".openfoodfacts.org/cgi/suggest.pl?lc="+ language + "&tagtype=" + tag_type
-	return autosuggest
+    autosuggest = "http://" + geography + ".openfoodfacts.org/cgi/suggest.pl?lc=" + language + "&tagtype=" + tag_type
+    return autosuggest
