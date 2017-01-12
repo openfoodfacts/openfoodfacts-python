138200e95fab40f334f72a8c8397108d57be21cc
Frank Rousseau <frank.rousseau@free.fr>
2017-01-12 21:13:45 +0100

    Fix PEP8 in autosuggest module

--- openfoodfacts/autosuggest.py
+++ openfoodfacts/autosuggest.py
@@ -3,5 +3,6 @@
 
 def get_autosuggest_url(tag_type, language, geography):
     base_url = "https://" + geography + ".openfoodfacts.org"
-    autosuggest = base_url + "/cgi/suggest.pl?lc=" + language + "&tagtype=" + tag_type
+    autosuggest = base_url + "/cgi/suggest.pl?lc=" + language
+    autosuggest += "&tagtype=" + tag_type
     return autosuggest
