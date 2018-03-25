eb2950e61dacd7f695cb89889be6b449eeed44ad
Anubhav-Bhargava <anubhavbhargava007@gmail.com>
2018-03-25 15:16:22 +0530

    Update utils.py

--- openfoodfacts/utils.py
+++ openfoodfacts/utils.py
@@ -34,17 +34,17 @@ def login_into_OFF():
 	
 
 def fetch(path, json_file=True):
-    """
-    Fetch data at a given path assuming that target match a json file and is
-    located on the OFF API.
-    """
-    if json_file:
-        path = "%s%s.json" % (API_URL, path)
-    else:
-        path = "%s%s" % (API_URL, path)
+	"""
+	Fetch data at a given path assuming that target match a json file and is
+	located on the OFF API.
+	"""
+	if json_file:
+	path = "%s%s.json" % (API_URL, path)
+	else:
+	path = "%s%s" % (API_URL, path)
 
-    response = requests.get(path)
-    return response.json()
+	response = requests.get(path)
+	return response.json()
 
 
 def get_ocr_json_url_for_an_image(first_three_digits,
@@ -52,16 +52,16 @@ def get_ocr_json_url_for_an_image(first_three_digits,
                                   third_three_digits,
                                   fourth_three_digits,
                                   image_name):
-    """
-    Get the URL of a JSON file given a barcode in 4 chunks of 3 digits and an
-    image name (1, 2, 3, front_fr...).
-    """
-    url = "https://world.openfoodfacts.org/images/products/"
-    url += "%s/%s/%s/%s/%s.json" % (
-        first_three_digits,
-        second_three_digits,
-        third_three_digits,
-        fourth_three_digits,
-        image_name
-    )
-    return url
+	"""
+	Get the URL of a JSON file given a barcode in 4 chunks of 3 digits and an
+	image name (1, 2, 3, front_fr...).
+	"""
+	url = "https://world.openfoodfacts.org/images/products/"
+	url += "%s/%s/%s/%s/%s.json" % (
+	first_three_digits,
+	second_three_digits,
+	third_three_digits,
+	fourth_three_digits,
+	image_name
+	)
+	return url
