a0d35adad42ea495ff9bb6d3556e1f146dec6a60
Anubhav-Bhargava <anubhavbhargava007@gmail.com>
2018-03-25 17:22:25 +0530

    Update utils.py

--- openfoodfacts/utils.py
+++ openfoodfacts/utils.py
@@ -9,25 +9,25 @@ def login_into_OFF():
     #Get username and pasword
     username = raw_input('Username:')
     pswd = getpass.getpass('Password:')
-    
+
     payload = {'user_id': username, 'password': pswd}
-    
+
     #create a requests session
     with requests.Session() as c:
-        
+
         #post the username and password on the website
         r = c.post(API_URL, data=payload)
-        
+
         #get the complete html text
         complete_html = r.text
-        
+
         #check if login is successfull
         if re.search('You are connected as', complete_html):
             print "Login Successfull"
         else:
             print "Incorrect username or password"
             exit()
-        
+
         #Return the session object
         return c        
 
