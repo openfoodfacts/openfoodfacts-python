453764cf561ed8e867349f888e57e749108ebc85
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2023-07-19 17:55:34 +0200

    fix: remove root logger

--- openfoodfacts/__init__.py
+++ openfoodfacts/__init__.py
@@ -2,10 +2,6 @@ from .api import API
 from .dataset import ProductDataset, get_dataset
 from .ocr import OCRResult
 from .types import APIConfig, APIVersion, Country, DatasetType, Environment, Flavor
-from .utils import get_logger
-
-# Instantiate root logger
-get_logger()
 
 __all__ = [
     "API",
