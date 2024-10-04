fd2f8f2faad1f670e316374924cf42eed8d4fab0
Raphaël Bournhonesque <raphael@bournhonesque.eu>
2024-10-04 17:36:11 +0200

    fix: don't assume Pillow is available

--- openfoodfacts/utils.py
+++ openfoodfacts/utils.py
@@ -351,7 +351,7 @@ def get_image_from_url(
     error_raise: bool = True,
     session: Optional[requests.Session] = None,
     return_struct: bool = False,
-) -> Union[ImageDownloadItem, Image.Image, None]:
+) -> Union[ImageDownloadItem, "Image.Image", None]:
     """Fetch an image from `image_url` and load it.
 
     :param image_url: URL of the image to load.
