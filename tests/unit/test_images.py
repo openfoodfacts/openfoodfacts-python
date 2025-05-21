224c38cc202e94ba2ecb6ba5a68ba7f8609c4c49
Raphaël Bournhonesque <raphael0202@users.noreply.github.com>
2025-05-21 09:50:25 +0200

    fix: add new function to convert images to old schema (#327)

--- tests/unit/test_images.py
+++ tests/unit/test_images.py
@@ -3,6 +3,7 @@ from typing import Optional
 import pytest
 
 from openfoodfacts.images import (
+    convert_to_legacy_schema,
     extract_barcode_from_url,
     extract_source_from_url,
     generate_image_url,
@@ -156,3 +157,293 @@ def test_generate_json_ocr_url(code, image_id, flavor, environment, expected):
 )
 def test_extract_barcode_from_url(url, expected):
     assert extract_barcode_from_url(url) == expected
+
+
+IMAGES_WITH_LEGACY_SCHEMA = {
+    "1": {
+        "sizes": {
+            "100": {"h": 100, "w": 56},
+            "400": {"h": 400, "w": 225},
+            "full": {"h": 3555, "w": 2000},
+        },
+        "uploaded_t": "1490702616",
+        "uploader": "user1",
+    },
+    "2": {
+        "sizes": {
+            "100": {"h": 100, "w": 56},
+            "400": {"h": 400, "w": 225},
+            "full": {"h": 3555, "w": 2000},
+        },
+        "uploaded_t": "1490702690",
+        "uploader": "user2",
+    },
+    "3": {
+        "sizes": {
+            "100": {"h": 100, "w": 56},
+            "400": {"h": 400, "w": 225},
+            "full": {"h": 3555, "w": 2000},
+        },
+        "uploaded_t": "1490702705",
+        "uploader": "user2",
+    },
+    "front_fr": {
+        "angle": None,
+        "geometry": "0x0-0-0",
+        "imgid": "3",
+        "normalize": "0",
+        "rev": "27",
+        "sizes": {
+            "100": {"h": 100, "w": 75},
+            "200": {"h": 200, "w": 150},
+            "400": {"h": 400, "w": 300},
+            "full": {"h": 1200, "w": 901},
+        },
+        "white_magic": "0",
+        "x1": None,
+        "x2": None,
+        "y1": None,
+        "y2": None,
+    },
+    "ingredients_fr": {
+        "angle": None,
+        "geometry": "0x0-0-0",
+        "imgid": "1",
+        "normalize": "0",
+        "ocr": 1,
+        "orientation": "0",
+        "rev": "29",
+        "sizes": {
+            "100": {"h": 40, "w": 100},
+            "200": {"h": 81, "w": 200},
+            "400": {"h": 162, "w": 400},
+            "full": {"h": 1200, "w": 2972},
+        },
+        "white_magic": "0",
+        "x1": None,
+        "x2": None,
+        "y1": None,
+        "y2": None,
+    },
+    "nutrition_fr": {
+        "angle": None,
+        "geometry": "0x0-0-0",
+        "imgid": "2",
+        "normalize": "0",
+        "ocr": 1,
+        "orientation": "0",
+        "rev": "18",
+        "sizes": {
+            "100": {"h": 53, "w": 100},
+            "200": {"h": 107, "w": 200},
+            "400": {"h": 213, "w": 400},
+            "full": {"h": 1093, "w": 2050},
+        },
+        "white_magic": "0",
+        "x1": None,
+        "x2": None,
+        "y1": None,
+        "y2": None,
+    },
+}
+
+
+IMAGES_WITH_NEW_SCHEMA = IMAGES = {
+    "uploaded": {
+        "1": {
+            "sizes": {
+                "100": {
+                    "h": 100,
+                    "w": 56,
+                    "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/1.100.jpg",
+                },
+                "400": {
+                    "h": 400,
+                    "w": 225,
+                    "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/1.400.jpg",
+                },
+                "full": {
+                    "h": 3555,
+                    "w": 2000,
+                    "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/1.jpg",
+                },
+            },
+            "uploaded_t": "1490702616",
+            "uploader": "user1",
+        },
+        "2": {
+            "sizes": {
+                "100": {
+                    "h": 100,
+                    "w": 56,
+                    "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/2.100.jpg",
+                },
+                "400": {
+                    "h": 400,
+                    "w": 225,
+                    "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/2.400.jpg",
+                },
+                "full": {
+                    "h": 3555,
+                    "w": 2000,
+                    "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/2.jpg",
+                },
+            },
+            "uploaded_t": "1490702690",
+            "uploader": "user2",
+        },
+        "3": {
+            "sizes": {
+                "100": {
+                    "h": 100,
+                    "w": 56,
+                    "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/3.100.jpg",
+                },
+                "400": {
+                    "h": 400,
+                    "w": 225,
+                    "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/3.400.jpg",
+                },
+                "full": {
+                    "h": 3555,
+                    "w": 2000,
+                    "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/3.jpg",
+                },
+            },
+            "uploaded_t": "1490702705",
+            "uploader": "user2",
+        },
+    },
+    "selected": {
+        "front": {
+            "fr": {
+                "imgid": "3",
+                "rev": "27",
+                "sizes": {
+                    "100": {
+                        "h": 100,
+                        "w": 75,
+                        "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/front_fr.27.100.jpg",
+                    },
+                    "200": {
+                        "h": 200,
+                        "w": 150,
+                        "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/front_fr.27.200.jpg",
+                    },
+                    "400": {
+                        "h": 400,
+                        "w": 300,
+                        "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/front_fr.27.400.jpg",
+                    },
+                    "full": {
+                        "h": 1200,
+                        "w": 901,
+                        "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/front_fr.27.full.jpg",
+                    },
+                },
+                "generation": {
+                    "white_magic": "0",
+                    "x1": None,
+                    "x2": None,
+                    "y1": None,
+                    "y2": None,
+                    "normalize": "0",
+                    "angle": None,
+                    "geometry": "0x0-0-0",
+                },
+            },
+        },
+        "nutrition": {
+            "fr": {
+                "imgid": "2",
+                "rev": "18",
+                "sizes": {
+                    "100": {
+                        "h": 53,
+                        "w": 100,
+                        "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/nutrition_fr.18.100.jpg",
+                    },
+                    "200": {
+                        "h": 107,
+                        "w": 200,
+                        "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/nutrition_fr.18.200.jpg",
+                    },
+                    "400": {
+                        "h": 213,
+                        "w": 400,
+                        "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/nutrition_fr.18.400.jpg",
+                    },
+                    "full": {
+                        "h": 1093,
+                        "w": 2050,
+                        "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/nutrition_fr.18.full.jpg",
+                    },
+                },
+                "generation": {
+                    "white_magic": "0",
+                    "x1": None,
+                    "x2": None,
+                    "y1": None,
+                    "y2": None,
+                    "normalize": "0",
+                    "ocr": 1,
+                    "orientation": "0",
+                    "angle": None,
+                    "geometry": "0x0-0-0",
+                },
+            },
+        },
+        "ingredients": {
+            "fr": {
+                "imgid": "1",
+                "rev": "29",
+                "sizes": {
+                    "100": {
+                        "h": 40,
+                        "w": 100,
+                        "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/ingredients_fr.29.100.jpg",
+                    },
+                    "200": {
+                        "h": 81,
+                        "w": 200,
+                        "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/ingredients_fr.29.200.jpg",
+                    },
+                    "400": {
+                        "h": 162,
+                        "w": 400,
+                        "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/ingredients_fr.29.400.jpg",
+                    },
+                    "full": {
+                        "h": 1200,
+                        "w": 2972,
+                        "url": "https://images.openfoodfacts.org/images/products/326/385/950/6216/ingredients_fr.29.full.jpg",
+                    },
+                },
+                "generation": {
+                    "white_magic": "0",
+                    "x1": None,
+                    "x2": None,
+                    "y1": None,
+                    "y2": None,
+                    "normalize": "0",
+                    "ocr": 1,
+                    "orientation": "0",
+                    "angle": None,
+                    "geometry": "0x0-0-0",
+                },
+            }
+        },
+    },
+}
+
+
+@pytest.mark.parametrize(
+    "images,expected_result",
+    [
+        ({}, {}),
+        (IMAGES_WITH_LEGACY_SCHEMA, IMAGES_WITH_LEGACY_SCHEMA),
+        (IMAGES_WITH_NEW_SCHEMA, IMAGES_WITH_LEGACY_SCHEMA),
+    ],
+)
+def test_convert_to_legacy_schema(images, expected_result):
+    assert convert_to_legacy_schema(images) == expected_result
