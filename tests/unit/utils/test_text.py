e383de11cc4aff9177887ee007aa0996b7240ff2
Raphaël Bournhonesque <raphael0202@users.noreply.github.com>
2025-05-30 10:56:18 +0200

    fix: fix issue in map_to_canonical_id function (#332)
    Solves #331

--- tests/unit/utils/test_text.py
+++ tests/unit/utils/test_text.py
@@ -1,6 +1,6 @@
 import pytest
 
-from openfoodfacts.utils.text import get_tag
+from openfoodfacts.utils.text import get_tag, replace_lang_prefix
 
 
 @pytest.mark.parametrize(
@@ -22,3 +22,30 @@ from openfoodfacts.utils.text import get_tag
 )
 def test_get_tag(value: str, output: str):
     assert get_tag(value) == output
+
+
+@pytest.mark.parametrize(
+    "tag,new_lang_prefix,output",
+    [
+        ("fr:gesiers", "en", "en:gesiers"),
+        ("fr:gesiers", "fr", "fr:gesiers"),
+        ("fr:gesiers", "ar", "ar:gesiers"),
+        ("en:apple", "fr", "fr:apple"),
+        ("xx:sashimi", "it", "it:sashimi"),
+        ("xx:sashimi", "xx", "xx:sashimi"),
+    ],
+)
+def test_replace_lang_prefix(tag, new_lang_prefix, output):
+    assert replace_lang_prefix(tag, new_lang_prefix) == output
+
+
+def test_replace_lang_prefix_invalid_new_lang_prefix():
+    with pytest.raises(ValueError, match="new_lang_prefix 'a' must be a 2-letter code"):
+        replace_lang_prefix("en:apples", "a")
+
+
+def test_replace_lang_prefix_invalid_tag():
+    with pytest.raises(
+        ValueError, match="tag 'e:apples' has an invalid language prefix"
+    ):
+        replace_lang_prefix("e:apples", "fr")
